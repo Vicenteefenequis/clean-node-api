4a9c3b824ea2b9b16e1ab255fce36a4f2c2a50be
Vicente Nascimento <vicente19981@live.com>
2022-08-09 09:50:06 -0300

    refactor: move bcript-adapter files to a folder

--- src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -1,12 +1,13 @@
 import bcrypt from 'bcrypt'
-import { HashComparer } from '../../data/protocols/cryptography/hash-comparer'
-import { Hasher } from '../../data/protocols/cryptography/hasher'
+import { HashComparer } from '../../../data/protocols/cryptography/hash-comparer'
+import { Hasher } from '../../../data/protocols/cryptography/hasher'
 
 export class BcryptAdapter implements Hasher, HashComparer {
   constructor (private readonly salt: number) {}
 
   async compare (value: string, hash: string): Promise<boolean> {
-    return await bcrypt.compare(value, hash)
+    const isValid = await bcrypt.compare(value, hash)
+    return isValid
   }
 
   async hash (value: string): Promise<string> {
