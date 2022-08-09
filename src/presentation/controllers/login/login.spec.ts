00f4bef107c93b291aba65f98d9bc2660319d30e
Vicente Nascimento <vicente19981@live.com>
2022-08-08 21:34:24 -0300

    refactor: move Validation to a protocols folder

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -2,7 +2,7 @@ import { MissingParamError } from '../../errors'
 import { badRequest, ok, serverError, unauthorized } from '../../helpers/http/http-helper'
 import { Authentication, HttpRequest } from './login-protocols'
 import { LoginController } from './login'
-import { Validation } from '../../helpers/validators/validation'
+import { Validation } from '../../protocols/validation'
 
 const makeAuthentication = (): Authentication => {
   class AuthenticationStub implements Authentication {
